c7431506d6377b61ffbf76d5a1de68b398b4301d
biocdeniz <carlitre@gmail.com>
2019-02-04 17:06:40 -0500

    Added bootstrap functionality

--- src/jupyter_run.sh
+++ src/jupyter_run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-
-export SPARK_HOME=/usr/lib/spark
-export PYSPARK_PYTHON=python3
-export HAIL_HOME=/opt/hail-on-AWS-spot-instances
-
-export PYTHONPATH="/home/hadoop/hail-python.zip:$SPARK_HOME/python:${SPARK_HOME}/python/lib/py4j-src.zip"
-echo "PYTHONPATH: ${PYTHONPATH}"
-
-export PYSPARK_PYTHON=python3
-echo "PYSPARK_PYTHON: ${PYSPARK_PYTHON}"
-
-
-JAR_PATH="/home/hadoop/hail-all-spark.jar:/usr/share/aws/emr/emrfs/lib/emrfs-hadoop-assembly-2.22.0.jar"
-export PYSPARK_SUBMIT_ARGS="--conf spark.driver.extraClassPath='$JAR_PATH' --conf spark.executor.extraClassPath='$JAR_PATH' pyspark-shell"
-echo "PYSPARK_SUBMIT_ARGS: ${PYSPARK_SUBMIT_ARGS}"
+#
+# export SPARK_HOME=/usr/lib/spark
+# export PYSPARK_PYTHON=python3
+# export HAIL_HOME=/opt/hail-on-AWS-spot-instances
+#
+# export PYTHONPATH="/home/hadoop/hail-python.zip:$SPARK_HOME/python:${SPARK_HOME}/python/lib/py4j-src.zip"
+# echo "PYTHONPATH: ${PYTHONPATH}"
+#
+# export PYSPARK_PYTHON=python3
+# echo "PYSPARK_PYTHON: ${PYSPARK_PYTHON}"
+#
+#
+# JAR_PATH="/home/hadoop/hail-all-spark.jar:/usr/share/aws/emr/emrfs/lib/emrfs-hadoop-assembly-2.22.0.jar"
+# export PYSPARK_SUBMIT_ARGS="--conf spark.driver.extraClassPath='$JAR_PATH' --conf spark.executor.extraClassPath='$JAR_PATH' pyspark-shell"
+# echo "PYSPARK_SUBMIT_ARGS: ${PYSPARK_SUBMIT_ARGS}"
 
 sudo mkdir -p $HOME/.jupyter
 cp /opt/hail-on-AWS-spot-instances/src/jupyter_notebook_config.py $HOME/.jupyter/
--- src/run_when_new_instance_added.sh
+++ src/run_when_new_instance_added.sh
@@ -16,11 +16,11 @@ else
      echo "Updating cluster"
      scp -o "StrictHostKeyChecking no" /home/hadoop/.ssh/id_rsa ${WORKERIP}:/home/hadoop/.ssh/id_rsa
      scp /home/hadoop/.ssh/authorized_keys ${WORKERIP}:/home/hadoop/.ssh/authorized_keys
-     # Install Python 3
-     scp /opt/hail-on-AWS-spot-instances/src/install_python36.sh hadoop@${WORKERIP}:/tmp/install_python36.sh
-     ssh hadoop@${WORKERIP} "sudo ls -al /tmp/install_python36.sh"
-     ssh hadoop@${WORKERIP} "sudo /tmp/install_python36.sh"
-     ssh hadoop@${WORKERIP} "python3 --version"
+     # # Install Python 3
+     # scp /opt/hail-on-AWS-spot-instances/src/install_python36.sh hadoop@${WORKERIP}:/tmp/install_python36.sh
+     # ssh hadoop@${WORKERIP} "sudo ls -al /tmp/install_python36.sh"
+     # ssh hadoop@${WORKERIP} "sudo /tmp/install_python36.sh"
+     # ssh hadoop@${WORKERIP} "python3 --version"
      # Distribute Hail files
      scp /home/hadoop/hail-* ${WORKERIP}:/home/hadoop/
      sudo grep -i privateip /mnt/var/lib/info/*.txt | sort -u | cut -d "\"" -f 2 > /tmp/t1.txt
